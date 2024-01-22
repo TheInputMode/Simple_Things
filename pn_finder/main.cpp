2bbcb10b626cdf1e8526ab6134623f05cd5cd73e
TheInputMode <136199334+TheInputMode@users.noreply.github.com>
2024-01-22 04:43:55 -0800

    prime number finder
    whatever :|

---
+++ pn_finder/main.cpp
@@ -0,0 +1,86 @@
+// DATE CREATED: Jan 22, 2024
+// LAST CHANGED: Jan 22, 2024, 05:38
+#include "std_lib_facilities.h"
+
+int main()
+{
+	/*
+		Doing this shit helps the thinking process A LOT!
+	
+	define variables
+
+	loop that tests each number from 2-max (defined above)
+	  filter out numbers that are divisible by 2 <-------------------- This step was skipped when I created the loop
+	    loop through list of primes
+		  is the current number divisible by any number in the list
+	        if it is, it isn't prime
+		      unset flag that prime was found (if it was set before)
+			  break out of current loop
+			if it isn't, it is prime
+			  set flag that prime was found
+		was a prime found?
+		  add the current number to the list
+
+	*/
+
+	/*
+	*	We opted for Roman-Numerals defining our loops as opposed to the usual
+	*	i, j, and k variables... kinda confusing, and probably not happening again :|
+	*/
+
+	size_t end = 0;
+	std::cout << "Enter end of range above zero: ";
+	std::cin >> end;
+
+	const auto time_start{ std::chrono::steady_clock::now() };
+
+/*
+*	This is the main algorithm, everything else is either printing or time-keeping (aside from input above)
+*/
+///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	bool found_prime = false;
+	std::vector<size_t> primes; // Stored in a vector. Can be written to a file or whatever, but I don't know how to do that :(
+	primes.push_back(2);        // start on 2 because fuck you
+	
+	for (size_t I = 3; I <= end; I += 2) { // this skips the check for even numbers (only example of NOT using ++ I've seen so far)
+		for (size_t II = 0; II < primes.size(); II++) {
+			if (primes[II] > I / 2) {
+				break;
+			}
+			if (I % primes[II] == 0) {
+				found_prime = false;
+				break;
+			}
+			else {
+				found_prime = true;
+			}
+		}
+		if (found_prime) {
+			primes.push_back(I);
+		}
+	}
+
+///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	const auto time_end{ std::chrono::steady_clock::now() };
+
+	// print those primes
+	for (size_t I = 0; I < primes.size(); I++) {
+		std::cout << primes[I] << '\n';
+	}
+
+	const auto time_end_total{ std::chrono::steady_clock::now() };
+
+	// calculate times
+	const std::chrono::duration<double> elapsed_time{ time_end - time_start };
+	const std::chrono::duration<double> total_time{ time_end_total - time_start };
+	std::cout
+		<< "Total number of primes from 0 to " << end << ": " << primes.size() << std::endl
+		<< "Calculation Time: " << elapsed_time.count() << " seconds\n"
+		<< "Total Time: " << total_time.count() << " seconds\n\n";
+
+	keep_window_open();
+
+	return 0;
+}
---
+++ pn_finder/std_lib_facilities.h
@@ -0,0 +1,374 @@
+/*
+*						DISCLAIMER
+* 
+*	This file was originally sourced from https://www.stroustrup.com/programming_support.html. 
+*	under "Supporting material:".
+* 
+*	I edited this file to make it easier for me to read through, and added functions to help
+*	solve problems found in Programming Principles and Practice 2nd Edition.
+* 
+*	My edits are documented in the UNOFFICIAL CHANGES: list, listed January 2024 and later.
+* 
+*	Some changes won't be documented (mainly because they're too small and insignificant)
+*	and will be acknowledged when a larger change is made to the file.
+*/
+
+/*
+?						std_lib_facilities.h
+*	
+*	simple "Programming: Principles and Practice using C++ (second edition)" course header to
+*	be used for the first few weeks.
+*	It provides the most common standard headers (in the global namespace)
+*	and minimal exception/error support.
+*	
+*	Students: please don't try to understand the details of headers just yet.
+*	All will be explained. This header is primarily used so that you don't have
+*	to understand every concept all at once.
+*	
+*	By Chapter 10, you don't need this file and after Chapter 21, you'll understand it
+*	
+* 
+* NOTE:
+* 
+*	If this changelog gets too massive, put it in a separate .md file for formatting and
+*	readability.
+* 
+* 
+* CHANGELOG:
+* 
+*	Revised: Apr 25, 2010 - simple_error() added.
+*
+*	Revised: Nov 25, 2013 - remove support for pre-C++11 compilers, use C++11: <chrono>.
+*	Revised: Nov 28, 2013 - add a few container algorithms.
+*
+*	Revised: Jun 8,  2014 - added #ifndef to workaround Microsoft C++11 weakness.
+*
+*	Revised: Feb 2,  2015 - randint() can now be seeded (see exercise 5.13).
+*
+*	Revised: Aug 3,  2020 - a cleanup removing support for ancient compilers.
+*
+* 
+* UNOFFICIAL CHANGES:
+* 
+*	Revised: Jan 20, 2024 - simple reformatting to make reading easier, also removed using namespace std statement
+*							and changed #ifndef directive.
+* 
+*	Revised: Jan 21, 2024 - added DMath namespace. DMath will hold math functions not found within the standard
+*							library, or experimental math function redefines. Also commented out the vector #define
+*							inbetween Vector and String structs, I wasn't able to create vector objects because
+*							of it.
+*/
+
+#ifndef PPP__std_lib__H
+#define PPP__std_lib__H
+
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+#include <list>
+#include <forward_list>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <array>
+#include <regex>
+#include <random>
+#include <stdexcept>
+#include <chrono>
+
+
+namespace DMath { ///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	/*
+	*	I opted to go with double instead of int even though factorial only operates on
+	*	integers. Now the largest number (besides inf, if you accept that) that can be
+	*	represented is 170!.
+	*
+	*	This function should only take integers, double is used for the range, show
+	*	error for input of double, but still calculate the result.
+	*
+	* USAGE:
+	*	DMath::factorial(5.0);
+	* or
+	*	double variable = DMath::factorial(5.5) <- this will truncate, but works
+	*
+	*/
+	double factorial(double factoriand)
+	{
+		if (factoriand == 0) { /// Math says so, so I say so. :|
+			return 1.0;
+		}
+		if (factoriand < 0) {
+			std::cerr
+				<< "[ERROR] Factorial operating on negative number...\n\a";
+			return NAN;
+		}
+		double result = static_cast<int>(factoriand); // cast to truncate these variables
+		double temp = static_cast<int>(factoriand);
+		if (result != factoriand) {
+			std::cerr
+				<< "[WARNING] Using floating-point type on factorial operator...\n"
+				<< "          Truncation will occur!\n\a";
+		}
+		for (int i = 1; i < static_cast<int>(factoriand); i++) {
+			result *= temp - 1;
+			temp--;
+		}
+		return result;
+	}
+
+	/*
+	*	Floor function that casts number to int then returns it, effectively
+	*	flooring the number.
+	*	Is this good? I don't know :|
+	*/
+	template <typename UserType>
+	constexpr UserType floor(UserType number)
+	{
+		int temp = static_cast<int>(number);
+		if (temp == number) { // If the number is already an integer, this function shouldn't work... right?
+			return static_cast<UserType>(temp);
+		}
+		return static_cast<UserType>(temp);
+	}
+
+	/*
+	*	Check above...
+	*/
+	template <typename UserType>
+	constexpr UserType ceil(UserType number)
+	{
+		int temp = static_cast<int>(number);
+		if (temp == number) {
+			return temp;
+		}
+		temp++;
+		return static_cast<UserType>(temp);
+	}
+
+}///namespace DMath ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+typedef long Unicode;
+
+
+template<class T>
+std::string to_string(const T& t)
+{
+	std::ostringstream os;
+	os << t;
+	return os.str();
+}
+
+struct Range_error : std::out_of_range
+{
+	int index;
+	Range_error(int i) :
+		out_of_range("Range error: " + to_string(i)), index(i)
+	{}
+};
+
+
+template< class T>
+struct Vector : public std::vector<T>
+{
+	using size_type = typename std::vector<T>::size_type;
+	using std::vector<T>::vector;	// inheriting constructor
+
+	T& operator[](unsigned int i) // rather than return at(i);
+	{
+		if (i<0 || this->size() <= i) {
+			throw Range_error(i);
+		}
+		return std::vector<T>::operator[](i);
+	}
+	const T& operator[](unsigned int i) const
+	{
+		if (i<0 || this->size() <= i) {
+			throw Range_error(i);
+		}
+		return std::vector<T>::operator[](i);
+	}
+};
+
+// disgusting macro hack to get a range checked vector:
+//#define vector Vector										// was this even necessary?
+
+// trivially range-checked string (no iterator checking):
+struct String : std::string
+{
+	using size_type = std::string::size_type;
+
+	char& operator[](unsigned int i) // rather than return at(i);
+	{
+		if (i<0 || size() <= i) {
+			throw Range_error(i);
+		}
+		return std::string::operator[](i);
+	}
+
+	const char& operator[](unsigned int i) const
+	{
+		if (i<0 || size() <= i) {
+			throw Range_error(i);
+		}
+		return std::string::operator[](i);
+	}
+};
+
+
+namespace std
+{
+	template<> struct hash<String>
+	{
+		size_t operator()(const String& s) const
+		{
+			return hash<std::string>()(s);
+		}
+	};
+
+} // of namespace std
+
+
+struct Exit : std::runtime_error
+{
+	Exit() :
+		std::runtime_error("Exit")
+	{}
+};
+
+
+// error() simply disguises throws:
+inline void error(const std::string& s)
+{
+	throw std::runtime_error(s);
+}
+
+inline void error(const std::string& s, const std::string& s2)
+{
+	error(s + s2);
+}
+
+inline void error(const std::string& s, int i)
+{
+	std::ostringstream os;
+	os << s << ": " << i;
+	error(os.str());
+}
+
+
+template<class T> char* as_bytes(T& i)	// needed for binary I/O
+{
+	void* addr = &i;	// get the address of the first byte of memory used to store the object
+	return static_cast<char*>(addr); // treat that memory as bytes
+}
+
+
+inline void keep_window_open()
+{
+	std::cin.clear();
+	std::cout << "Please enter a character to exit\n";
+	char ch;
+	std::cin >> ch;
+	return;
+}
+
+inline void keep_window_open(std::string s)
+{
+	if (s == "") {
+		return;
+	}
+	std::cin.clear();
+	std::cin.ignore(120, '\n');
+	for (;;)
+	{
+		std::cout << "Please enter " << s << " to exit\n";
+		std::string ss;
+		while (std::cin >> ss && ss != s)
+		{
+			std::cout << "Please enter " << s << " to exit\n";
+		}
+		return;
+	}
+}
+
+// error function to be used (only) until error() is introduced in Chapter 5:
+inline void simple_error(std::string s)	// write ``error: s and exit program
+{
+	std::cerr << "error: " << s << '\n';
+	keep_window_open();		// for some Windows environments
+	exit(1);
+}
+
+// make std::min() and std::max() accessible on systems with antisocial macros:
+#undef min
+#undef max
+
+// run-time checked narrowing cast (type conversion). See ???.
+template<class R, class A>
+R narrow_cast(const A& a)
+{
+	R r = R(a);
+	if (A(r) != a) {
+		error(std::string("info loss"));
+	}
+	return r;
+}
+
+inline std::default_random_engine& get_rand()
+{
+	static std::default_random_engine ran;	// note: not thread_local
+	return ran;
+};
+
+inline void seed_randint(int s)
+{
+	get_rand().seed(s);
+}
+
+inline int randint(int min, int max)
+{
+	return std::uniform_int_distribution<>{min, max}(get_rand());
+}
+
+inline int randint(int max)
+{
+	return randint(0, max);
+}
+
+template<typename C>
+using Value_type = typename C::value_type;
+
+template<typename C>
+using Iterator = typename C::iterator;
+
+template<typename C>
+void sort(C& c)
+{
+	std::sort(c.begin(), c.end());
+}
+
+template<typename C, typename Pred>
+void sort(C& c, Pred p)
+{
+	std::sort(c.begin(), c.end(), p);
+}
+
+template<typename C, typename Val>
+Iterator<C> find(C& c, Val v)
+{
+	return std::find(c.begin(), c.end(), v);
+}
+
+template<typename C, typename Pred>
+Iterator<C> find_if(C& c, Pred p)
+{
+	return std::find_if(c.begin(), c.end(), p);
+}
+
+#endif //PPP__std_lib__H
